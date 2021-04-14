4ba3b745a13f4accb6ecf5a66897cee2dfe9450c
Mascart <Mr.Mascart.kl@yandex.ru>
2021-04-14 19:25:39 +0300

    Camera resize script try 2

---
+++ Arkanoid_pr/Assets/Scripts/CameraSize.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSize : MonoBehaviour
+{
+    private const float TargetSizeX = 1280.0f;
+    private const float TargetSizeY = 720.0f;
+    private const float HalfSize = 200.0f; // половина высоты в пикселях
+
+    private void Awake()
+    {
+        CameraResize();
+    }
+    private void CameraResize()
+    {
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = TargetSizeX / TargetSizeY;
+
+        if(screenRatio >= targetRatio)
+        {
+            Resize();
+        }
+        else
+        {
+            float differentSize = targetRatio / screenRatio;
+            Resize(differentSize);
+        }
+    }
+
+    private void Resize(float differentSize = 1.0f)
+    {
+        Camera.main.orthographicSize = TargetSizeY / HalfSize * differentSize;
+    }
+}
