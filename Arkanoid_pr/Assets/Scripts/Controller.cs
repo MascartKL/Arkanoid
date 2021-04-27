0810b2f6449471c1573af043635f943e9cb7b97e
Maksim <maksim.dorofeev.95@mail.ru>
2021-04-28 00:41:15 +0300

    Отскок и шестерёнка

--- Arkanoid_pr/Assets/Scripts/Controller.cs
+++ Arkanoid_pr/Assets/Scripts/Controller.cs
@@ -8,35 +8,22 @@ public class Controller : MonoBehaviour
     // Start is called before the first frame update
     public GameObject MenuInGame;
 
-	private void Start()
-	{
-		
-	}
-	// Update is called once per frame
-	void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape))
-		{
-           pause();
-		}
-    }
-
-	private int pause()
+	public void  pause()
 	{
 		if (Time.timeScale != 0)
 		{
 			Time.timeScale = 0;
 			MenuInGame.SetActive(true);
-			return 0;
+			return;
 		}
 
 		if (Time.timeScale == 0)
 		{
 			Time.timeScale = 1;
 			MenuInGame.SetActive(false);
-			return 0;
+			return;
 		}
 
-		return 0;
+		return;
 	}
 }
--- Arkanoid_pr/Assets/Scripts/MoveBall.cs
+++ Arkanoid_pr/Assets/Scripts/MoveBall.cs
@@ -7,6 +7,7 @@ public class MoveBall : MonoBehaviour
     Vector2 dir;
 
     bool active = false;
+    Vector2 velocity;
     public Rigidbody2D rb;
     private float BallSpeed = 100f;
 
@@ -15,18 +16,24 @@ public class MoveBall : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    
+
     void Update()
     {
-    
-        if (Input.GetMouseButtonDown(0)&& !active)
-		{
+        if (Input.GetMouseButtonDown(0) && !active)
+        {
             dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             active = true;
-            rb.AddForce(dir * BallSpeed);   
+            rb.AddForce(dir * BallSpeed);
         }
     }
 
-   
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Board")
+        {
+            rb.velocity = new Vector2(gameObject.transform.position.x * -1f, gameObject.transform.position.y);
+        }
+    }
 }
+
+
